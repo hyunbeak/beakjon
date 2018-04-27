bd929ba6050adf9912ddff331b0d44bc52320509
hyunbeak <38645337+hyunbeak@users.noreply.github.com>
2018-04-27 09:04:42 +0900

    reupdate

--- snain.c
+++ snain.c
@@ -1,28 +1,54 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void main(){
-	
-	int **a;
-	int size=0;
-	int i,j,x=0;
-	scanf("%d",&size);
-	a=(int(**))malloc (size*sizeof(int*));
-	for (i=0;i<size;+i){
-		a[i]=(int*)malloc(size*sizeof(int));
-		
+void main()
+{
+   int i, j,a=0;
+   int size=0,tsize=0;
+   int cntx=0,cnty=0,c=1;
+   int **aNum;
+   scanf("%d",&size);
+	tsize=size;
+// 유동 할당하기
+   aNum = (int **)malloc(size * sizeof(int *));
+   for(i=0; i<size; ++i)
+   {
+        aNum[i] = (int *)malloc( size * sizeof(int) );
+   }
+
+   while(size){
+	for(i=0;i<size;i++)
+	{
+		aNum[cntx][cnty]=a++;
+		cnty+=c;
 	}
+		cntx+=c;
+		cnty-=c;
+		size--;
+	for(i=0;i<size;i++)
+	{
+		aNum[cntx][cnty]=a++;
+		cntx+=c;
+	}
+		cntx-=c;	
+		c*=-1;
+		cnty+=c;
+		
+		
+   }
+   
 
-	//a[0]=(int(*)[])malloc (sizeof(int*)*size);
-	for(i=0;i<size;i++){
-		a[i]=(int*)malloc(size*sizeof(int));
-      		for(j=0;j<size;j++){
-			a[i][j]=x++;
- 			printf("|%2d|",a[i][j]);
-		}
-      	}
-	      printf("\n");
-	
 
-	
-	return 0;
+// 결과 출력하기
+   for(i=0; i<tsize; ++i)
+   {
+        for(j=0; j<tsize; ++j) printf("  |%12d|", aNum[i][j]);
+        printf("\n");
+   }
+
+// 할당 해제하기
+   for(i=0; i<tsize; ++i) free(aNum[i]);
+   free(aNum);
+
 }
+       
